b95b541eb7887ecac9e307101ec2bd21c628ae83
shaliniyadav <shaliniyadav.ait@gmail.com>
2025-09-02 15:21:07 +0530

    add controller to get list of employees

---
+++ src/main/java/com/tw/employee/controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package com.tw.employee.controller;
+
+import com.tw.employee.entity.Employee;
+import com.tw.employee.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService theEmployeeService) {
+        employeeService = theEmployeeService;
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model theModel) {
+        List<Employee> theEmployees = employeeService.findAll();
+        theModel.addAttribute("employees", theEmployees);
+        return "list-employees";
+    }
+}
