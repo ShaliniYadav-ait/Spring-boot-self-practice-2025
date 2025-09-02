9a47620fb3a1914858f937cbdbb16c8d372432bf
shaliniyadav <shaliniyadav.ait@gmail.com>
2025-09-02 22:09:51 +0530

    sort employees by lastname

--- src/main/java/com/tw/employee/repository/EmployeeRepository.java
+++ src/main/java/com/tw/employee/repository/EmployeeRepository.java
@@ -3,5 +3,8 @@ package com.tw.employee.repository;
 import com.tw.employee.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    List<Employee> findAllByOrderByLastNameAsc();
 }
